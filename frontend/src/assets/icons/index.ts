cc9b52c3a7d6261ab8871831d258950445d319de
huykg1112 <92528393+huykg1112@users.noreply.github.com>
2025-03-13 10:33:35 +0700

    chore: various fixes and improvements

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import Delivery from "./delivery-truck.png";
+import Natural from "./natural.png";
+import Organic from "./organic.png";
+import Support from "./support.png";
+
+export { Delivery, Natural, Organic, Support };
